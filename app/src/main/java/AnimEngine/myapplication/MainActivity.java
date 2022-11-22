66f0461a748dc1c108296bc4b0fba0729555e7ec
Benjamin Saldman <benjaminsaldman@gmail.com>
2022-11-22 23:53:31 +0200

    firebase connection

--- app/src/main/java/AnimEngine/myapplication/MainActivity.java
+++ app/src/main/java/AnimEngine/myapplication/MainActivity.java
@@ -1,14 +1,39 @@
 package AnimEngine.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-
+    FirebaseFirestore f;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        f=FirebaseFirestore.getInstance();
+        Map<String,Object> users=new HashMap<>();
+        users.put("FirstName","Ben");
+        users.put("LastName","Ani Tom");
+        f.collection("users").add(users).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+            @Override
+            public void onSuccess(DocumentReference documentReference) {
+                Toast.makeText(getApplicationContext(),"tom shamen",Toast.LENGTH_SHORT).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getApplicationContext(),"ben hadebil",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
